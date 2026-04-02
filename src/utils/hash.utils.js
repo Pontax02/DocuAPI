d99721be36e46212a719eb346fc9a2e5528ee0fe
Pontax02 <ppontanilla07@gmail.com>
2026-04-02 12:00:47 +0200

    feat(utils): add function to compute SHA-256 hash of a file

---
+++ src/utils/hash.utils.js
@@ -0,0 +1,8 @@
+import crypto from 'crypto';
+import fs from 'fs';
+
+
+export const getFileHash = (filePath) => {
+    const buffer = fs.readFileSync(filePath);
+    return crypto.createHash('sha256').update(buffer).digest('hex');
+};
