9c45e02f4e3f46575f6f1e0f22a7b564e8963097
Pontax02 <ppontanilla07@gmail.com>
2026-04-07 16:32:18 +0200

    feat(docs): add Swagger UI setup for API documentation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import helmet from "helmet";
+import swaggerUi from "swagger-ui-express";
 import { corsOptions } from "./config/cors.js";
 import { rateLimitOptions } from "./config/rateLimit.js";
 import { errorHandler, notFoundHandler } from "./middlewares/error.middleware.js";
 import validateRoutes from "./routes/validate.routes.js";
 import healthRouter from "./routes/health.routes.js";
 import adminRouter from "./routes/admin.routes.js";
+import { swaggerDocument } from "./config/swagger.js";
 
 
 
@@ -23,6 +25,7 @@ app.use(express.json({ limit: `10mb` }));
 app.use('/api', validateRoutes);
 app.use('/api', healthRouter);
 app.use('/api/admin', adminRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(notFoundHandler);
 
